8c05c13633870b29bd91ea05609e6ef020e3b1f5
akhalyavin <akhalyavin@phoenixit.ru>
2022-08-01 15:46:33 +0300

    first commit

---
+++ src/main/java/com/example/educationakhlyavinapi/EducationAkhlyavinApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.educationakhlyavinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationAkhlyavinApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationAkhlyavinApiApplication.class, args);
+    }
+
+}
